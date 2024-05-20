2570f708f3b9aac9e78a81dbd78f26ec4ff33828
mcleshiy16 <mcleshiy16@gmail.com>
2024-05-20 21:22:51 +0700

    added access to db

---
+++ api/v1/database/access.py
@@ -0,0 +1,6 @@
+def GetConnectionString():
+    return "postgresql://postgres:postgres@localhost:5432/DigitalUp_Practice_IMITMediaContent"
+
+
+def GetConnectionString_render():
+    return "postgresql://postgres:password/database_name"
